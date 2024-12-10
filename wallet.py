b90da8254af2b0076327edc5f121548244f18c96
AnderG2003 <agorocica002@ikasle.ehu.eus>
2024-12-10 13:01:39 +0100

    Parte 2

--- wallet.py
+++ wallet.py
@@ -10,3 +10,12 @@ class Wallet:
 
     def remove_balance(self, val):
         self.balance = self.balance - val
+
+    def increase_balance(self):
+        self.balance = self.balance + 1000
+
+if __name__ == "__main__":
+    myWallet = Wallet(0)
+    myWallet.increase_balance()
+    val = myWallet.get_balance()
+    print("El balance tiene que ser 1000 y es: ", val)
